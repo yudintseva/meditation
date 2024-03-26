143e748878d6737a3d7057229bdc8799dc3f2bfb
yudintseva <145755358+yudintseva@users.noreply.github.com>
2024-03-27 00:40:27 +0300

    Add files via upload

---
+++ javascripts/java-script.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    $(function() {
+        $(".const") .draggable();
+        });
+
+
+        $('.krug1').click(function() {
+            $(this).toggleClass('krug1_anima');
+        });
+});
+
+
